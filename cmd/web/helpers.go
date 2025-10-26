a8d7b6194e672584ff80525a010a0ab8b7ac0623
T460 <jacobgeorge08@gmail.com>
2025-10-26 22:19:28 +0530

    added a bytes to buffer to catch runtime errors before writing to Response Writer

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -35,10 +36,16 @@ func (app *application) render(
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
+
 }
