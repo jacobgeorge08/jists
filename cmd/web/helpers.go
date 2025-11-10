6d918c4fcc4ed91ffc831639c286dfb811933419
T460 <jacobgeorge08@gmail.com>
2025-11-10 06:56:31 +0530

    set up the session manager to put() and pop()

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -56,6 +56,7 @@ func (app *application) render(
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
